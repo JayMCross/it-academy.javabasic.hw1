4beba4b136a4401be8da7b34afb31d2b455a8812
JayMCross <71892437+JayMCross@users.noreply.github.com>
2020-09-29 17:40:42 +0300

    feat: finished tasks  1, 2, 6, 9, 10, 12

--- src/main/java/com/rakovets/course/javabasics/practice/operators/Task12.java
+++ src/main/java/com/rakovets/course/javabasics/practice/operators/Task12.java
@@ -19,5 +19,12 @@ public class Task12 {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
         // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+        int sec = playingTimeInSeconds % 60;
+        int m = (playingTimeInSeconds - sec) / 60;
+        int min = m % 60;
+        int h = (m - min) / 60;
+        int hours = h % 24;
+        int days = (h - hours) / 24;
+        System.out.println(days+" "+hours+":"+min+":"+sec);
     }
 }
